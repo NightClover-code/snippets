1f76870051d2704d65a3ecdeb2277aef5ec6de7f
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2024-02-24 03:11:34 +0100

    feat: dark mode config w/ next-themes & tailwind css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <main>{children}</main>
         </ThemeProvider>
       </body>
     </html>
