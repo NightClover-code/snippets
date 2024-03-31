3f15605483b26fb2149d1fd9514503f46a347995
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2024-03-31 07:24:32 +0000

    feat: adding loading spinner to create snippet

---
+++ src/app/snippets/new/loading.tsx
@@ -0,0 +1,9 @@
+import { LoadingSpinner } from '@/components/ui/loading';
+
+export default function SnippetLoadingPage() {
+  return (
+    <div className="flex items-center justify-center">
+      <LoadingSpinner />
+    </div>
+  );
+}
